327ba62800f9abbcd20cdfcdce8d4e1b162e0a48
martin <guevara.mrtn@gmail.com>
2020-11-15 18:08:39 +0100

    changed navbar layout and added timer functionality for testing

--- js/use_webworker.js
+++ js/use_webworker.js
@@ -16,31 +16,3 @@ worker.addEventListener("message", function(messageEvent) {
   log.textContent = messageEvent.data;
 
 });
-
-
-function startTimer(duration, display) {
-
-  var timer = duration, minutes, seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
-    }
-  }, 1000);
-}
-document.getElementById("realTimer").onclick= function () {
-  var twentyfiveMinutes = 60 * 25,
-      display = document.querySelector('#displayTime');
-  startTimer(twentyfiveMinutes, display);
-
-};
-
-
-

faf49caf3768667c62e48322d93e59fabb7d4cf5
Islam Hemida <islam.hemida@edu.fh-joanneum.at>
2020-11-28 17:49:21 +0100

    Added Card instead List Index.html

--- js/webworker.js
+++ js/webworker.js
@@ -1,20 +1,23 @@
 /**************
  * WEB WORKER *
  **************/
-self.addEventListener("message", function(messageEvent) {
-
+self.addEventListener("message", function (messageEvent) {
   if (messageEvent.data === "hello") {
     self.postMessage("Hello to you too !");
   }
 
   if (messageEvent.data === "recurring task") {
-
     for (let i = 0; i < 1500; i++) {
-      setTimeout(() => self.postMessage(function(){
-        var dn=new Date();
-        return 'Time left ' + dn.getMinutes() + ":" + dn.getSeconds();
-      }()), i * 1000);
+      setTimeout(
+        () =>
+          self.postMessage(
+            (function () {
+              var dn = new Date();
+              return "Time left " + dn.getMinutes() + ":" + dn.getSeconds();
+            })()
+          ),
+        i * 1000
+      );
     }
   }
-
 });

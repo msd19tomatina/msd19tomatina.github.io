0935ddaa267d269545fa48a92bf0a77a6ece5ee0
martin-gk <>
2020-12-06 10:47:07 +0100

    modified html js and json files

--- js/gruppen.js
+++ js/gruppen.js
@@ -1,25 +1,29 @@
 var id = "5fb4eaf370896ed1b1080791"
+var template = document.getElementById("card-template");
+var cards = document.getElementById("memberRows");
 
 $.ajax({
     url: datafilepath,
     dataType:"json",
     type:"get",
     cache: false,
-    success: [function (items) {
+    success: function (items) {
         $(items.user).each(function(i, myUser){
-
+            var card = template.content.cloneNode(true);
             $(myUser.groups).each(function(i, myGroup){
-                document.getElementById("memberRows").innerHTML=myUser.username;
                 if (myGroup.id === id) {
                     document.getElementById("Gruppe").innerHTML=myGroup.name;
-                    fillMembers(myUser)
+                    card.querySelector(".group-member-name").innerHTML=myUser.username;
+                    card.querySelector(".group-member-pomodoro").innerHTML=myGroup.pomodoros;
+                    cards.appendChild(card);
                 }
+
             })
 
 
     })
 
-        }]
+        }
     })
 ;
 
@@ -32,7 +36,7 @@ function fillMembers (members) {
     for (let i = 0; i < members.length; i++) {
         // Create the list item:
         let item = document.createElement('div');
-        item.className = "col-sm-6"
+        item.className = "card col-sm-6"
         // item.className="list-group-item"
 
         // Set its contents:
